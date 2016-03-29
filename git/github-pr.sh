afd3480dc38277877fcd02d4e4708ea26c38df91
Nikita Lutsenko <nlutsenko@me.com>
2016-03-28 20:31:20 -0700

    Add GitHub pull request checkout script.

---
+++ git/github-pr.sh
@@ -0,0 +1,22 @@
+#
+# Git Checkout a Pull Request locally.
+#
+# Usage: gfpr 8
+# Where 8 is the number of pull request (can be found right after the PR's title).
+# @author Ahmad Awais github.com/ahmadawais
+#
+function ghpr(){
+  echo "${whitef}———————————————————${reset}"
+    echo "${whiteb} ${blackf}0. Fetching the pull request...${reset}"
+
+    git fetch origin pull/"${1}"/head:pull_"${1}"
+
+    echo "${whiteb} ${blackf}1. PR fetched creating a branch...${reset}"
+
+    git checkout pull_"${1}"
+
+    echo "${whiteb} ${blackf}2. Checking out to a new PR branch...${reset}"
+
+    echo "${greenb} ${blackf}3. PR Branch Created!!!${reset}"
+  echo "${whitef}———————————————————${reset}"
+}
