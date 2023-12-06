81947b068bb170beaa2390671959feb282c7a3ef
Nikita Lutsenko <git@nlutsenko.me>
2023-12-06 10:41:30 -0800

    Lint brew.sh

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if ! command -v brew >/dev/null; then
-  echo "Installing homebrew"
+	echo "Installing homebrew"
 	install_script=$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)
 	ruby -e "${install_script}"
 	unset install_script
