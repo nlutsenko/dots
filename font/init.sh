28d6bf92b42ea81d94bf07328bc5aee37cf322ec
Nikita Lutsenko <git@nlutsenko.me>
2023-12-06 10:58:34 -0800

    Lint font/init.sh

--- font/init.sh
+++ font/init.sh
@@ -1,8 +1,11 @@
-# OSX-only stuff. Abort if not OSX.
-if [[ ! `uname` == 'Darwin' ]]; then
-  return
+#!/usr/bin/env bash
+
+## Check if we are running on macOS
+
+if [[ $(uname || true) != 'Darwin' ]]; then
+	return
 fi
 
 # Copy fonts
 e_header "Copying fonts (${#setdiffC[@]})"
-cp -f $HOME/.dotfiles/font/* $HOME/Library/Fonts 
+cp -f "${HOME}/.dotfiles/font/*" "${HOME}/Library/Fonts"
