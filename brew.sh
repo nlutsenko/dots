74960ad97ece4513b5ea1b247cf0f375c8aa19f0
Nikita Lutsenko <git@nlutsenko.me>
2023-12-06 10:58:05 -0800

    Lint brew.sh

--- brew.sh
+++ brew.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+## Check if we are running on macOS
+if [[ $(uname || true) != 'Darwin' ]]; then
+	return
+fi
+
 if ! command -v brew >/dev/null; then
 	echo "Installing homebrew"
 	install_script=$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)
